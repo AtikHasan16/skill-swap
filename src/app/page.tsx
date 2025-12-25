2394a7a4651dc2f549488b5540be4f05e4bfac56
Hasan MD Atik <akhasannaeem@gmail.com>
2025-12-25 14:24:46 +0600

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <header>
-      <Navbar></Navbar>
-    </header>
+    <>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </>
   );
 }
