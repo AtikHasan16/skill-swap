a81d28d9695d8db0e8b1660e3d3132855b6fd507
Hasan MD Atik <akhasannaeem@gmail.com>
2025-12-27 12:11:09 +0600

    customised some desing

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const BackgroundGradient = () => {
   return (
     <div className="fixed inset-0 z-[-2] h-full w-full overflow-hidden bg-[#000000]">
-      Left Glow
+      
       <div className="absolute -top-30 -left-[250px] h-[200px] w-[200px] rounded-full bg-[#00ffff] blur-[150px]" />
 
       {/* Right Glow */}
